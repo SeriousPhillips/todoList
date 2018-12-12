2c43a4acd69944c16704e9ea6341dc260f1723ea
SeriousPhillips <SeriousPhillips@gmail.com>
2018-12-12 13:18:10 +0700

    Update urls.py

--- app_todoLst/urls.py
+++ app_todoLst/urls.py
@@ -12,12 +12,7 @@ urlpatterns = [
     path('sign_in_process/', views.sign_in_process, name='sign_in_process'),
     path('sign_un_process/', views.sign_up_process, name='sign_up_process'),
     path('tasks/', views.tasks, name='tasks' ),
-   # path('preauth/', views.preauth),
-   # path('auth/', views.auth, name='auth' ),
-    path('addtask/',views.addTask,name='addTask'),
-    path('cnhgtasks/',views.chngTasks,name='chngTasks'),
 
-    #path('tt/',views.tt,name='tt')]
 ]
     
 
